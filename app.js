327ba65206e005b360e512efe6c0ad11d9b8060b
sylvamadu <sylvamaduneche@gmail.com>
2021-10-05 14:36:45 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,224 @@
+const form = document.querySelector("#form")
+const usernameEl = document.querySelector("#Username")
+const emailEl = document.querySelector("#Email")
+const passwordEl = document.querySelector("#Password")
+const confirmPasswordEl = document.querySelector("#ConfirmPass")
+
+const info = document.querySelector('.info')
+
+form.addEventListener("submit", function (e){
+    //prevent the form from submitting
+    e.preventDefault()
+
+    // validate forms
+    let isUsernameValid = checkUsername(),
+        isEmailValid = checkEmail(),
+        isPasswordValid = checkPassword(),
+        isConfirmPasswordValid = checkConfirmPassword();
+
+    let isFormValid = isUsernameValid &&
+        isEmailValid &&
+        isPasswordValid &&
+        isConfirmPasswordValid;
+
+    // submit to the server if the form is valid
+    if (isFormValid) {
+        api()
+        //reset()
+        //alert('your form is succesfully submitted')
+        
+    }
+})
+
+
+//POST request using Promises:
+
+const api =()=>{
+    fetch('https://6113d42dcba40600170c1c89.mockapi.io/datas', {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+   body: JSON.stringify({
+     // your expected POST request payload goes here
+      name: usernameEl.value,
+      email: emailEl.value,
+      password: passwordEl.value,
+      })
+})
+  .then(res => res.json())
+  .then(data => {
+   // enter you logic when the fetch is successful
+    console.log(data)
+    reset()
+  })
+  .catch(error => {
+  // enter your logic for when there is an error (ex. error toast)
+   console.log(error)
+  })  
+}
+const debounce = (fn, delay = 500) => {
+    let timeoutId;
+    return (...args) => {
+        // cancel the previous timer
+        if (timeoutId) {
+            clearTimeout(timeoutId);
+        }
+        // setup a new timer
+        timeoutId = setTimeout(() => {
+            fn.apply(null, args)
+        }, delay);
+    };
+};
+
+form.addEventListener('input', debounce(function (e) {
+    switch (e.target.id) {
+        case 'Username':
+            checkUsername();
+            break;
+        case 'Email':
+            checkEmail();
+            break;
+        case 'Password':
+            checkPassword();
+            break;
+        case 'ConfirmPass':
+            checkConfirmPassword();
+            break;
+    }
+}));
+
+/*utilities
+ field required
+ password length
+ emailvalid
+ password strength
+*/
+const reset =()=>{
+    usernameEl.value = " "
+    emailEl.value=" "
+    passwordEl.value=""
+    confirmPasswordEl.value = ""
+    resetBorders()
+}
+const resetBorders=()=>{
+    usernameEl.parentElement.classList.remove('success')
+    emailEl.parentElement.classList.remove('success')
+    passwordEl.parentElement.classList.remove('success')
+    confirmPasswordEl.parentElement.classList.remove('success')
+}
+const isRequired = value => value === " "? false:true
+const isBetween = (length,min,max)=> length < min || length > max?false:true
+const isEmailValid = email =>{
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+}
+const isPasswordSecure = (password) => {
+    const re = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})");
+    return re.test(password);
+};
+
+/* develop functions that shows error and success*/
+
+//showerror function
+const showError =(input, message)=>{
+    //get the form-field element
+    const formField = input.parentElement
+    
+    //remove success and add error message
+    formField.classList.remove('success')
+    formField.classList.add('error')
+    
+
+    // show the error message
+    const error = formField.querySelector('small')
+    error.textContent = message
+}
+
+//showsuccess function
+const showSuccess =(input)=>{
+    //get the form-field element
+    const formField = input.parentElement
+    
+    //remove success and add error message
+    formField.classList.remove('error')
+    formField.classList.add('success')
+    
+
+    // show the error message
+    const error = formField.querySelector('small')
+    error.textContent = " "
+}
+
+//Develop input field validating functions
+
+//checkusername
+const checkUsername = ()=>{
+    let valid = false;
+    const min = 3,
+        max = 25;
+    const username = usernameEl.value.trim();
+
+    if (!isRequired(username)) {
+        showError(usernameEl, 'Username cannot be blank.');
+    } else if (!isBetween(username.length, min, max)) {
+        showError(usernameEl, `Username must be between ${min} and ${max} characters.`)
+    } else {
+        showSuccess(usernameEl);
+        valid = true;
+    }
+    return valid;
+}
+
+//checkemail
+const checkEmail = () => {
+    let valid = false;
+    const email = emailEl.value.trim();
+    if (!isRequired(email)) {
+        showError(emailEl, 'Email cannot be blank.');
+    } else if (!isEmailValid(email)) {
+        showError(emailEl, 'Email is not valid.')
+    } else {
+        showSuccess(emailEl);
+        valid = true;
+    }
+    return valid;
+}
+
+//checkpassword
+const checkPassword = () => {
+
+    let valid = false;
+
+    const password = passwordEl.value.trim();
+
+    if (!isRequired(password)) {
+        showError(passwordEl, 'Password cannot be blank.');
+    } else if (!isPasswordSecure(password)) {
+        showError(passwordEl, 'Password must has at least 8 characters that include at least 1 lowercase character, 1 uppercase characters, 1 number, and 1 special character in (!@#$%^&*)');
+    } else {
+        showSuccess(passwordEl);
+        valid = true;
+    }
+
+    return valid;
+};
+
+//check confirmpassword
+const checkConfirmPassword = () => {
+    let valid = false;
+    // check confirm password
+    const confirmPassword = confirmPasswordEl.value.trim();
+    const password = passwordEl.value.trim();
+
+    if (!isRequired(confirmPassword)) {
+        showError(confirmPasswordEl, 'Please enter the password again');
+    } else if (password !== confirmPassword) {
+        showError(confirmPasswordEl, 'Confirm password does not match');
+    } else {
+        showSuccess(confirmPasswordEl);
+        valid = true;
+    }
+
+    return valid;
+};
